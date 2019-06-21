4a233b46416edcfd00b849af7cbb4f1f0f3ad462
Rory Murray <rory.christian.murray@gmail.com>
2019-06-21 12:24:58 -0400

    setup SmurfList.js

---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getSmurfs } from "../actions";
+
+class SmurfList extends Component {
+  componentDidMount() {
+    console.log("Smurfs being staged for Smurf Prison Bus transportation");
+    this.props.getSmurfs();
+  }
+
+  render() {
+    console.log(this.props.smurfs);
+    return (
+      <div>
+        {this.props.smurfs.map(smurf => {
+          return <SmurfCard smurf={smurf} key={smurf.id} />;
+        })}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  console.log("SmurfList mapStateToProps state", state);
+  return {
+    smurfs: state.smurfs,
+    fetching: state.fetching
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { getSmurfs }
+)(SmurfList);
